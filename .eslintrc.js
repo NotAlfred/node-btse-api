a48b078d408ad4bc547683d60138a7b589a28d6b
NotAlfred <40529467+NotAlfred@users.noreply.github.com>
2020-07-05 14:49:53 +0200

    config files

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
